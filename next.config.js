b84145f54061aa2feb0b5f08b01f6f6841a825fc
Hiram Abjar <hiramabjar@github.com>
2025-02-03 02:55:14 -0300

    Atualiza schema do Prisma com correções de relações

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
